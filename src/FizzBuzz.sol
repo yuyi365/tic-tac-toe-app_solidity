50f5bc4bad47cc43f334ae2c232ae881437220b1
Yuyi <yuyi365@gmail.com>
2022-04-06 15:08:14 -0400

    initial commit

---
+++ src/FizzBuzz.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.10;
+
+contract FizzBuzz {
+    function fizzbuzz(uint n) public returns (string memory) {
+        return "fizz";
+    }
+}
