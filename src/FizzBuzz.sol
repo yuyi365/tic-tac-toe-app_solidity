89d2bbc2f1f5b85775ded1dea97ba1f95cef6097
Yuyi <yuyi365@gmail.com>
2022-04-13 15:01:46 -0400

    started game

--- src/FizzBuzz.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.10;
-
-contract FizzBuzz {
-    function fizzbuzz(uint n) public returns (string memory) {
-        return "fizz";
-    }
-}
